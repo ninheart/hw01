75dc3fc9d0ef4f327c9643e520d9a6f33eaa60dc
Avilez-dev-11 <avilez@rpi.edu>
2023-09-18 22:06:49 -0400

    Understanding Provided Code
    Added the sample.lambda given sample input for testing.
    Added names to README.
    Added the original comments back from given file for the assignment.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # hw01
 RPI Programming Languages Fall 2023
+
+Group Members: 
+Yifu Liu 
+Zuriel Aviles
+
+
--- main.hs
+++ main.hs
@@ -16,14 +16,39 @@ betaReduce :: Lexp -> Lexp
 -- eta reduction
 etaConvert :: Lexp -> Lexp
 
+-- You will need to write a reducer that does something more than
+-- return whatever it was given, of course!
 reducer :: Lexp -> Lexp
 reducer lexp = lexp
 
 start :: Lexp -> Lexp
 start lexp = etaConvert (betaReduce lexp)
 -- read command line arguments
-main = do 
-	args <- getArgs
-	let inFile = case args of { x:_ -> x; _ -> "input.lambda" }
-	let outFile = case args of { x:y:_ -> y; _ -> "output.lambda"}
-	runProgram inFile outFile reducer
+
+-- Haskell representation of lambda expression
+-- data Lexp = Atom String | Lambda String Lexp | Apply Lexp  Lexp 
+
+-- Given a filename and function for reducing lambda expressions,
+-- reduce all valid lambda expressions in the file and output results.
+-- runProgram :: String -> (Lexp -> Lexp) -> IO ()
+
+-- This is the identity function for the Lexp datatype, which is
+-- used to illustrate pattern matching with the datatype. "_" was
+-- used since I did not need to use bound variable. For your code,
+-- however, you can replace "_" with an actual variable name so you
+-- can use the bound variable. The "@" allows you to retain a variable
+-- that represents the entire structure, while pattern matching on
+-- components of the structure.
+id' :: Lexp -> Lexp
+id' v@(Atom _) = v
+id' lexp@(Lambda _ _) = lexp
+id' lexp@(Apply _ _) = lexp 
+
+-- Entry point of program
+main = do
+    args <- getArgs
+    let inFile = case args of { x:_ -> x; _ -> "input.lambda" }
+    let outFile = case args of { x:y:_ -> y; _ -> "output.lambda"}
+    -- id' simply returns its input, so runProgram will result
+    -- in printing each lambda expression twice. 
+    runProgram inFile outFile reducer
