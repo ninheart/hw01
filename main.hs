3d5ee14cff2db70997f4ca38f59d9623fa489c33
Charlie <charlieliu1016@gmail.com>
2023-09-23 21:39:22 -0400

    Add beta reduction and eta conversion but not finished

--- main.hs
+++ main.hs
@@ -3,52 +3,66 @@ import PA1Helper
 import System.Environment
 
 
--- Use beta reduction recurively 
--- alpha renaming
-alphaRename :: Lexp -> [String] -> Lexp
+-- return a list of expressions. 
+allVariables :: Lexp -> [Lexp]
+allVariables v@(Atom _) = [v]
+allVariables lexp@(Lambda x expr) = allVariables expr
+allVariables (Apply expr1 expr2) = allVariables expr1 ++ allVariables expr2
 
+remove :: (Eq a) => [a] -> a -> [a]
+remove [] _ = []
+remove list@(h:t) x
+    | x == h = remove t x
+    | otherwise = h: remove t x
+
+findFreeVariables :: Lexp -> [Lexp]
+findFreeVariables v@(Atom _) = [v]
+findFreeVariables lexp@(Lambda x expr) = findFreeVariables expr `remove` Atom x
+findFreeVariables lexp@(Apply expr1 expr2) = findFreeVariables expr1 ++ findFreeVariables expr2
+
+alphaRename :: Lexp -> [String] -> Lexp
+alphaRename v@(Atom _)_ = v
 
 
 -- beta reduction
+-- variable: no reduction 
+-- abstractions: Lambda (x expr), reduce expressions
+-- applications: (Apply expr1 , expr2) 
 betaReduce :: Lexp -> Lexp
+betaReduce v@(Atom _) = v
+-- recursively apply beta reduction
+betaReduce lexp@(Lambda x expr) = Lambda x (betaReduce expr)
+-- apply beta reduction to both expressions
+betaReduce lexp@(Apply expr1 expr2) =
+        let
+			expr3 = betaReduce expr1
+			expr4 = betaReduce expr2
+        -- check if alpha renaming is needed
+        -- if needed, rename the variable
+        	-- expr5 = alphaRename 
+        -- check if expr4 is a lambda abstraction 
+        in
+		case expr4 of
 
 
--- eta reduction
+-- eta conversion
 etaConvert :: Lexp -> Lexp
+-- etaConvert v@(Atom _) = v
+etaConvert (Lambda x (Apply f (Atom y))) = 
+    if x == y && not (x `elem` (map (\(Atom x) -> x) (findFreeVariables f))) 
+        then etaConvert f
+        else Lambda x (Apply (etaConvert f) (Atom y))
+etaConvert (Apply expr1 expr2) = Apply (etaConvert expr1) (etaConvert expr2)
+etaConvert (Lambda x expr) = Lambda x (etaConvert expr)
+etaConvert var@(Atom _) = var
 
--- You will need to write a reducer that does something more than
--- return whatever it was given, of course!
-reducer :: Lexp -> Lexp
-reducer lexp = lexp
 
 start :: Lexp -> Lexp
 start lexp = etaConvert (betaReduce lexp)
+
 -- read command line arguments
-
--- Haskell representation of lambda expression
--- data Lexp = Atom String | Lambda String Lexp | Apply Lexp  Lexp 
-
--- Given a filename and function for reducing lambda expressions,
--- reduce all valid lambda expressions in the file and output results.
--- runProgram :: String -> (Lexp -> Lexp) -> IO ()
-
--- This is the identity function for the Lexp datatype, which is
--- used to illustrate pattern matching with the datatype. "_" was
--- used since I did not need to use bound variable. For your code,
--- however, you can replace "_" with an actual variable name so you
--- can use the bound variable. The "@" allows you to retain a variable
--- that represents the entire structure, while pattern matching on
--- components of the structure.
-id' :: Lexp -> Lexp
-id' v@(Atom _) = v
-id' lexp@(Lambda _ _) = lexp
-id' lexp@(Apply _ _) = lexp 
-
--- Entry point of program
 main = do
-    args <- getArgs
-    let inFile = case args of { x:_ -> x; _ -> "input.lambda" }
-    let outFile = case args of { x:y:_ -> y; _ -> "output.lambda"}
-    -- id' simply returns its input, so runProgram will result
-    -- in printing each lambda expression twice. 
-    runProgram inFile outFile reducer
+        args <- getArgs
+        let inFile = case args of { x:_ -> x; _ -> "input.lambda" }
+        let outFile = case args of { x:y:_ -> y; _ -> "output.lambda"}
+        runProgram inFile outFile start
