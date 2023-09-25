021cb46f4e9fbf99932a23d598303debc34b1091
Charlie <charlieliu1016@gmail.com>
2023-09-25 16:57:23 -0400

    beta reduction and reducer function

--- main.hs
+++ main.hs
@@ -4,6 +4,7 @@
 
 import PA1Helper
 import System.Environment
+import Data.Time.Format.ISO8601 (yearFormat)
 
 -- given helper function freevars is used to extract free variables from lambda expression
 freevars :: Lexp -> [String]
@@ -55,41 +56,8 @@ alphaRename (Lambda var body) usedVars =
          unusedVar = getFreeVar var usedVars
 alphaRename lexp _ = lexp
 
--- utility function that recursively finds all variable in a lambda expression
-findAllVariables :: Lexp -> [String]
-findAllVariables (Atom v) = [v]
-findAllVariables (Lambda x expr) = x : findAllVariables expr
-findAllVariables (Apply expr1 expr2) = findAllVariables expr1 ++ findAllVariables expr2
-
-replaceAll :: Lexp -> Lexp -> Lexp -> Lexp
-replaceAll (Atom arg) value expr@(Atom e)
-    | e == arg = value
-    | otherwise = Atom e
-replaceAll (Atom arg) value expr@(Lambda x e) = Lambda x (replaceAll (Atom arg) value e)
-replaceAll (Atom arg) value expr@(Apply e1 e2) = Apply (replaceAll (Atom arg) value e1) (replaceAll (Atom arg) value e2)
-
--- beta reduction
--- variable: no reduction 
--- abstractions: Lambda (x expr), reduce expressions
--- applications: (Apply expr1 , expr2) 
-betaReduce :: Lexp -> Lexp
-betaReduce v@(Atom _) = v
--- recursively apply beta reduction
-betaReduce lexp@(Lambda x expr) = Lambda x (betaReduce expr)
--- apply beta reduction to both expressions
-betaReduce lexp@(Apply expr1 expr2) =
-   let
-    expr5 = betaReduce expr1
-    expr3 = betaReduce expr2
-    expr4 = alphaRename expr5 (findAllVariables expr5)
-    in
-    case expr4 of 
-      (Lambda x expr) -> betaReduce (replaceAll (Atom x) expr3 expr)
-      otherwise -> Apply expr4 expr3
-
 -- eta conversion
 etaConvert :: Lexp -> Lexp
--- etaConvert v@(Atom _) = v
 etaConvert (Lambda x (Apply f (Atom y))) =
    if x == y && notElem x (freevars f)
       then etaConvert f
@@ -99,12 +67,47 @@ etaConvert (Lambda x expr) = Lambda x (etaConvert expr)
 etaConvert var@(Atom _) = var
 
 
-start :: Lexp -> Lexp
-start lexp = etaConvert (betaReduce lexp)
+--    For variables no beta reduction is done
+--    For abstractions you beta reduce second argument
+--    For applications you beta reduce both arguments and check for alpha renaming
+beta :: String -> Lexp -> Lexp -> Lexp
+beta var body a@(Atom z)
+   | var == z = body
+   | otherwise = a
+beta var body (Apply f g) = Apply (beta var body f ) (beta var body g)
+beta x1 y1 f@(Lambda x2 y2) =
+    case () of
+        _
+            | x2 /= x1 && x2 `elem` freevars y1 ->
+                let
+                    alphaRenamed = alphaRename f (freevars y1)
+                in
+                    beta x1 y1 alphaRenamed
+            | x2 /= x1 && x2 `notElem` freevars y1 ->
+                let
+                    betaY2 = beta x1 y1 y2
+                in
+                    Lambda x2 betaY2
+            | otherwise -> f
+
+-- check if the expression is in its simple form
+simplifier :: Lexp -> Lexp -> Lexp
+simplifier x y = 
+   if x == y 
+      then y 
+      else reducer y
+
+-- kicks off the reduction process
+reducer :: Lexp -> Lexp
+reducer lexp@(Atom _) = lexp
+reducer lexp@(Apply f@(Lambda x y) v) = simplifier lexp (beta x (reducer v) y)
+reducer lexp@(Apply x y) = simplifier lexp (Apply (reducer x) (reducer y))
+reducer lexp@(Lambda x (Apply y v)) = simplifier lexp (etaConvert (Lambda x (Apply y v)))
+reducer lexp@(Lambda x e) = simplifier lexp (Lambda x (reducer e))
 
 -- read command line arguments
 main = do
-        args <- getArgs
-        let inFile = case args of { x:_ -> x; _ -> "input.lambda" }
-        let outFile = case args of { x:y:_ -> y; _ -> "output.lambda"}
-        runProgram inFile outFile start
+      args <- getArgs
+      let inFile = case args of { x:_ -> x; _ -> "input.lambda" }
+      let outFile = case args of { x:y:_ -> y; _ -> "output.lambda"}
+      runProgram inFile outFile reducer
